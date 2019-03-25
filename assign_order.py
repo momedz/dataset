3ce983abce88abe792d9d042cb18a6e0416d66fc
Unknown <karnklawmprasrerdh@gmail.com>
2019-03-25 08:36:42 +0700

    multi data

--- assign_order.py
+++ assign_order.py
@@ -4,15 +4,34 @@ client = MongoClient('10.80.37.160', 27017)
 database = client['queue']
 collection = database['task']
 
-collection.insert_one({
+data = [{
     'assign': 'hscmstream',
     'parameter': {
-        'radius': 0.30,
-        'np': 2
+        'radius': round(r * 0.05, 2),
+        'np': np
     },
     'dataset': {
-        'name': 'moon',
+        'name': 'AHCM_20k_2d',
         'normalize': False
     },
     'status': 'todo'
-})
+} for np in range(2, 21) for r in range(1,11)]
+
+
+
+# print(data.__len__())
+
+# data = [{
+#     'assign': 'hscmstream',
+#     'parameter': {
+#         'radius': 0.25,
+#         'np': 2
+#     },
+#     'dataset': {
+#         'name': 'HCM_5k',
+#         'normalize': False
+#     },
+#     'status': 'todo'
+# }]
+
+collection.insert_many(data)
